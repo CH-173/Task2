9c723d912aa8b3abcdde8678539f78936fa25d92
CH-173 <tcherk.33@yandex.ru>
2021-03-24 02:48:38 +0700

    add

---
+++ Distinction/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distinction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Введите количество элементов массива:");
+                try
+                {
+                    int arrLength = int.Parse(Console.ReadLine());
+                    if (arrLength < 0)
+                    {
+                        Console.WriteLine("Количество элементов массива не может быть отрицательным числом!");
+                    }
+                    else if (arrLength == 0)
+                    {
+                        Console.WriteLine("Количество элементов массива не может равняться нулю!");
+                    }
+                    else
+                    {
+                        int[] Elements = new int[arrLength];
+                        Console.WriteLine("Введите целочисленные элементы массива (по одному) в порядке возрастания");
+                        for (int i = 0; i < arrLength; i++)
+                        {
+                            Elements[i] = int.Parse(Console.ReadLine());
+                        }
+                        bool allRight = true;
+                        for (int f = 1; f < arrLength; f++)
+                        {
+                            if (Elements[f] < Elements[f - 1])
+                            {
+                                allRight = false;
+                                break;
+                            }
+                        }
+                        if (allRight == true)
+                        {
+                            int distinction = 1;
+                            for (int t = 1; t < arrLength; t++)
+                            {
+                                if (Elements[t] != Elements[t - 1])
+                                {
+                                    distinction++;
+                                }
+                            }
+                            Console.WriteLine("В массиве " + distinction + " уникальных элементов.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели элементы не в порядке возрастания!");
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Вы ввели не целое число элементов массива либо не целочисленный элемент массива!");
+                }
+            } 
+            while (true);
+
+            // Сложность алгоритма равна O(n)
+        }
+    }
+}
---
+++ Symmetry/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symmetry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Введите размер квадратной матрицы (ширина и высота):");
+                    int size = int.Parse(Console.ReadLine());
+                    if (size < 0)
+                    {
+                        Console.WriteLine("Ошибка! Вы ввели отрицательное значение!");
+                    }
+                    else if (size == 0)
+                    {
+                        Console.WriteLine("Ошибка! Вы ввели нулевое значение!");
+                    }
+                    else
+                    {
+                        double[,] Matrix = new double[size, size];
+                        for (int i = 0; i < size; i++)
+                        {
+                            Console.WriteLine("Введите числовые элементы " + (i + 1) + "-го ряда массива (по одному!):");
+                            for (int t = 0; t < size; t++)
+                            {
+                                Matrix[i, t] = double.Parse(Console.ReadLine());
+                            }
+                        }
+                        bool isSymmetric = true;
+                        for (int a = 0; a < size; a++)
+                        {
+                            for (int b = 0; b < size; b++)
+                            {
+                                if (Matrix[a, b] != Matrix[b, a])
+                                {
+                                    isSymmetric = false;
+                                }
+                            }
+                        }
+                        switch (isSymmetric)
+                        {
+                            case false:
+                                Console.WriteLine("Матрица, заданная вами, не симметрична!");
+                                break;
+                            default:
+                                Console.WriteLine("Матрица, заданная вами, симметрична!");
+                                break;
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка! Введенный вами размер квадратной матрицы не является целым числом, либо один из элементов матрицы не является числом.");
+                }
+            }
+            while (true);
+
+            // Сложность алгоритма равна O(N^2)
+        }
+    }
+}
